a7e0f130ea525b00b871fc22ecf85d8ecfb36202
Se AKi <seaki@sastudio.jp>
2021-01-31 23:56:29 +0900

    プロジェクト ファイルを追加します。

---
+++ SlimClock/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace SlimClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clockLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clockLabel
+            // 
+            this.clockLabel.AutoSize = true;
+            this.clockLabel.Location = new System.Drawing.Point(12, 9);
+            this.clockLabel.Name = "clockLabel";
+            this.clockLabel.Size = new System.Drawing.Size(109, 12);
+            this.clockLabel.TabIndex = 0;
+            this.clockLabel.Text = "9999/99/99 99:99:99";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(131, 28);
+            this.Controls.Add(this.clockLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label clockLabel;
+    }
+}
+
---
+++ SlimClock/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlimClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    Invoke((Action)(() =>
+                    {
+                        clockLabel.Text = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+                    }));
+                    await Task.Delay(1000);
+                }
+            });
+        }
+    }
+}
